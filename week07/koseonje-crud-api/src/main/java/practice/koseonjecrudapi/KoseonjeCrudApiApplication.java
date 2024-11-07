33d4dc672ecc6319269d49ede048f8d73044a0e9
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2024-11-07 18:48:20 +0900

    chore : 초기 문서 설정

---
+++ week07/koseonje-crud-api/src/main/java/practice/koseonjecrudapi/KoseonjeCrudApiApplication.java
@@ -0,0 +1,13 @@
+package practice.koseonjecrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoseonjeCrudApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoseonjeCrudApiApplication.class, args);
+    }
+
+}
